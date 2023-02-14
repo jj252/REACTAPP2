dcc07f8e88ecf70dc8e14855332b1c77589ace6e
Jeremy Jeremiah <jeremyjeremiah587@gmail.com>
2023-02-14 08:47:27 -0700

    demo version almost complete

--- src/utils/Logic.js
+++ src/utils/Logic.js
@@ -11,16 +11,11 @@ import life_lines from '../app/assets/img/lifelines.jpg';
 import life_lines2 from '../app/assets/img/lifelines2.jpg';
 import useSound from 'use-sound';
 import mainTheme from '../app/assets/sounds/1001000 music.mp3';
-
-const BoopButton = () => {
-    const [play,stop] = useSound(mainTheme);
-  
-    return <button onClick={play}>Boop!</button>;
-  };
-
+import finishedMusic from '../app/assets/sounds/main.mp3';
+import the_host from '../app/assets/img/Ask_the_Host.jpg';
 
 const Logic = () =>{
-    const [play] = useSound(mainTheme);
+    
     //set the progress to use to cycle through the game
     const [progress,setProgress] = useState(1);
     const [questionNumber,setQuestionNumber] = useState(1);
@@ -28,14 +23,15 @@ const Logic = () =>{
     const [bank,setBank] = useState(0);
     const [isFifty,setisFifty] = useState(false);
     const [isFiftyUsedOnce,setisFiftyUsedOnce] = useState(false);
-    let soundPlayed = false;
+    const [askTheHost,setaskTheHost] = useState(false);
+    const [askTheHost1,setaskTheHost1] = useState(false);
     
-        
-    
+    //responsible for playing the main theme song
+    const [play2, {stop}] = useSound(mainTheme);
     
     
 
-    // Counter use effect hook
+    //Responsible for setting up the timer if the timer is at 0 the game ends
     useEffect(() => {
     const timer =
         counter > 0 && setInterval(() => setCounter(counter - 1), 1000);
@@ -44,25 +40,34 @@ const Logic = () =>{
         }
         return () => {clearInterval(timer)};
         }, );
+    //50/50 is only given one second after that it defaults back to original
+    useEffect(() => {
+        const timer = setTimeout(() => {
+        setisFifty(false);
+        }, 1000);
+        return () => clearTimeout(timer);
+        }, [isFifty]);
 
-        useEffect(() => {
-            const timer = setTimeout(() => {
-                setisFifty(false);
-            }, 1000);
-            return () => clearTimeout(timer);
-          }, [isFifty]);
+        
 
+        //When the game ends the progress is set to zero and the game ends
         if(progress === 0){
-            console.log('THE PROGRESS IS AT 0');
+            {stop();}
+            return(
+            <>
+            <div className={style.game_over}>Game Over</div>
+            <div className={style.regular_text}>Thank you for playing Millionaire demo version</div>
+            <div className={style.regular_text2}>Your current Funds are ${bank}.00</div>
+            </>
+            )
             
         }
-
+        //When progress is set to 1 the first question is loaded
         else if(progress === 1){
-        {play()}
+        {play2()}
         return(
         <>
-        
-         <Questions prop={questionNumber}/>
+        <Questions prop={questionNumber}/>
         
           {!isFifty 
           ?(
@@ -72,7 +77,7 @@ const Logic = () =>{
           <Button className={style.option2} color="info" onClick={() => setProgress(0) }>B</Button>,
           <Button className={style.option3} color="info" onClick={() => setProgress(0) }>C</Button>,
           <Button className={style.option4} color="info" onClick={() => setProgress(0) }>D</Button>
-          <button onClick={play}>Boop!</button>;
+          
           </>
           )
           :(
@@ -87,20 +92,19 @@ const Logic = () =>{
           <Button className={style.yellow} color='warning'></Button>
           <div className={style.my_counter}>{counter}</div>
           <div className={style.bankCheck}>Money Earned ${bank}.00</div>
-          <div className={style.life_lines}><img src={life_lines}/></div>
+          
           {!isFiftyUsedOnce 
           ?<div className={style.life_lines2} onClick={() => {setisFifty(true);setisFiftyUsedOnce(true)}}><img src={life_lines2} alt='life lines'/></div>
           :console.log('ITS EQUAL TO TRUE')
           }
+          
           </>
         )
         
         }
+        //Loads the second set of questions and answers
         else if(progress === 2){
-            if (soundPlayed === false){
-            
-            soundPlayed = true;
-            }
+           
             return(
                 <>
                  <Questions prop={questionNumber}/>
@@ -109,7 +113,7 @@ const Logic = () =>{
                   <>
                   <Button className={style.option1} color="info" onClick={() => setProgress(0) }>A</Button>,
                   <Button className={style.option2} color="info" onClick={() => setProgress(0) }>B</Button>,
-                  <Button className={style.option3} color="info" onClick={() => {setQuestionNumber(3); setProgress(3);setCounter(30);setBank(100);} }>C</Button>,
+                  <Button className={style.option3} color="info" onClick={() => {setQuestionNumber(3); setProgress(3);setCounter(30);setBank(200);} }>C</Button>,
                   <Button className={style.option4} color="info" onClick={() => setProgress(0) }>D</Button>
                   
                   </>
@@ -119,7 +123,7 @@ const Logic = () =>{
                   <>
                   
                   <Button className={style.option1} color="info" onClick={() => setProgress(0) }>A</Button>,
-                  <Button className={style.option3} color="info" onClick={() => {setQuestionNumber(3); setProgress(3);setCounter(30);setBank(100);} }>C</Button>
+                  <Button className={style.option3} color="info" onClick={() => {setQuestionNumber(3); setProgress(3);setCounter(30);setBank(200);} }>C</Button>
                   </>
                   )
                   }
@@ -136,15 +140,16 @@ const Logic = () =>{
                 )
                 
                 }
+        //Loads the third set of questions and answers
         else if(progress === 3){
-        
+            
             return(
                 <>
                  <Questions prop={questionNumber}/>
                   {!isFifty 
                   ?(
                   <>
-                  <Button className={style.option1} color="info" onClick={() => {setQuestionNumber(4); setProgress(4);setCounter(30);setBank(100);} }>A</Button>,
+                  <Button className={style.option1} color="info" onClick={() => {setQuestionNumber(4); setProgress(4);setCounter(30);setBank(300);} }>A</Button>,
                   <Button className={style.option2} color="info" onClick={() => setProgress(0) }>B</Button>,
                   <Button className={style.option3} color="info" onClick={() => setProgress(0) }>C</Button>,
                   <Button className={style.option4} color="info" onClick={() => setProgress(0) }>D</Button>
@@ -154,7 +159,7 @@ const Logic = () =>{
                   :(
                    
                   <>
-                  <Button className={style.option1} color="info" onClick={() => {setQuestionNumber(4); setProgress(4);setCounter(30);setBank(100);} }>A</Button>,
+                  <Button className={style.option1} color="info" onClick={() => {setQuestionNumber(4); setProgress(4);setCounter(30);setBank(300);} }>A</Button>,
                   <Button className={style.option3} color="info" onClick={() => setProgress(0) }>C</Button>
                   </>
                   )
@@ -182,7 +187,7 @@ const Logic = () =>{
                   <>
                   <Button className={style.option1} color="info" onClick={() => setProgress(0) }>A</Button>,
                   <Button className={style.option2} color="info" onClick={() => setProgress(0) }>B</Button>,
-                  <Button className={style.option3} color="info" onClick={() => {setQuestionNumber(5); setProgress(5);setCounter(30);setBank(100);} }>C</Button>,
+                  <Button className={style.option3} color="info" onClick={() => {setQuestionNumber(5); setProgress(5);setCounter(30);setBank(500);} }>C</Button>,
                   <Button className={style.option4} color="info" onClick={() => setProgress(0) }>D</Button>
                   
                   </>
@@ -191,7 +196,7 @@ const Logic = () =>{
                    
                   <>
                   <Button className={style.option1} color="info" onClick={() => setProgress(0) }>A</Button>,
-                  <Button className={style.option3} color="info" onClick={() => {setQuestionNumber(5); setProgress(5);setCounter(30);setBank(100);} }>C</Button>
+                  <Button className={style.option3} color="info" onClick={() => {setQuestionNumber(5); setProgress(5);setCounter(30);setBank(500);} }>C</Button>
                   </>
                   )
                   }
@@ -217,7 +222,7 @@ const Logic = () =>{
                   ?(
                   <>
                   <Button className={style.option1} color="info" onClick={() => setProgress(0) }>A</Button>,
-                  <Button className={style.option2} color="info" onClick={() => {setQuestionNumber(6); setProgress(6);setCounter(30);setBank(100);} }>B</Button>,
+                  <Button className={style.option2} color="info" onClick={() => {setQuestionNumber(6); setProgress(6);setCounter(30);setBank(1000);} }>B</Button>,
                   <Button className={style.option3} color="info" onClick={() => setProgress(0) }>C</Button>,
                   <Button className={style.option4} color="info" onClick={() => setProgress(0) }>D</Button>
                   
@@ -226,7 +231,7 @@ const Logic = () =>{
                   :(
                    
                   <>
-                  <Button className={style.option2} color="info" onClick={() => {setQuestionNumber(6); setProgress(6);setCounter(30);setBank(100);} }>B</Button>,
+                  <Button className={style.option2} color="info" onClick={() => {setQuestionNumber(6); setProgress(6);setCounter(30);setBank(1000);} }>B</Button>,
                   <Button className={style.option3} color="info" onClick={() => setProgress(0) }>C</Button>
                   </>
                   )
@@ -255,14 +260,14 @@ const Logic = () =>{
                   <Button className={style.option1} color="info" onClick={() => setProgress(0) }>A</Button>,
                   <Button className={style.option2} color="info" onClick={() => setProgress(0) }>B</Button>,
                   <Button className={style.option3} color="info" onClick={() => setProgress(0) }>C</Button>,
-                  <Button className={style.option4} color="info" onClick={() => {setQuestionNumber(0); setProgress(0);setCounter(30);setBank(100);} }>D</Button>
+                  <Button className={style.option4} color="info" onClick={() => {setQuestionNumber(0); setProgress(0);setCounter(30);setBank(2000);} }>D</Button>
                   
                   </>
                   )
                   :(
                    
                   <>
-                  <Button className={style.option4} color="info" onClick={() => {setQuestionNumber(0); setProgress(0);setCounter(30);setBank(100);} }>D</Button>,
+                  <Button className={style.option4} color="info" onClick={() => {setQuestionNumber(0); setProgress(0);setCounter(30);setBank(2000);} }>D</Button>,
                   <Button className={style.option1} color="info" onClick={() => setProgress(0) }>A</Button>
                   </>
                   )
