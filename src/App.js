6bedb57a462256246d3ce19ee461a0d6a6f695f7
Jeremy Jeremiah <jeremyjeremiah587@gmail.com>
2023-02-21 15:25:38 -0700

    Cleaned Up Directory Structure

--- src/App.js
+++ src/App.js
@@ -3,28 +3,13 @@
 //import { Counter } from './features/counter/Counter';
 import './App.css';
 //import NucampLogo from './app/assets/img/logo.png';
-import CampsitesList from './features/campsites/CampsitesList';
-import { Container, Row, Col, Button } from 'reactstrap';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import {createElement,useState,useEffect,useRef} from 'react';
-import {CreatedElement} from './components/Misc';
-import {CreatedElement2} from './components/Misc';
-import {New_Button} from './components/Misc';
-import {New_Button2} from './components/Misc';
-import  My_Form  from './components/My_Form';
-import CampsiteDirectoryPage from './pages/CampsitesDirectoryPage';
-import {Routes, Route} from  'react-router-dom';
-import ContactPage from './pages/ContactPage';
-import HomePage from './pages/HomePage';
-import CampsiteDetailPage from './pages/CampsiteDetailPage';
-import AboutPage from './pages/AboutPage';
+
+import { Container, Row, Col,  } from 'reactstrap';
+
 import style from '../src/app/shared/StudentList.module.css';
-import AnimatedText from '../src/utils/AnimatedText';
-import Questions from './utils/Questions';
-import Choices from './utils/Choices';
+
 import Logic from './utils/Logic';
-import game_default2 from '../src/app/assets/img/game_default2.jpg';
+
 import game_default3 from '../src/app/assets/img/game_default3.jpg';
 
 
